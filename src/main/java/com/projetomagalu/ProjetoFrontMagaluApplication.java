a65224302335bde28c97c04eee153bbcf810aebe
HeyLet <leticiafaleiros9@gmail.com>
2021-05-12 19:52:39 -0300

    primeiro commit

---
+++ src/main/java/com/projetomagalu/ProjetoFrontMagaluApplication.java
@@ -0,0 +1,13 @@
+package com.projetomagalu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFrontMagaluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFrontMagaluApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetomagalu/ProjetoFrontMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetomagalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFrontMagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
